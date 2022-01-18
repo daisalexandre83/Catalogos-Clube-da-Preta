85321916bbf7e10f7f7a4945b0c8b33c32dd26d5
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-01-18 20:07:16 -0300

    Alterações no HTML, nas classes do CSS  e função no Javascript.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-$(".plano-clube").click(function () {
-    $(".plano-clube").hide();
-    $(".livros").show();
-})
+/* $(".btn-club-plan").click(function () {
+    $(".btn-club-plan").hide();
+    $(".books").show();
+}) */
+
+function showProducts() {
+    document.querySelector('.btn-club-plan').style.display = 'none';
+    document.querySelector('.books').style.display = 'block';
+}
+
+/* showProducts();
 
 $(".resumo-livro").click(function () {
     $(".texto-resumo-livro").show();
     $(".btn-escolher").hide();
+    $(".btn-close").show();
 })
+
+$(".btn-close").click(function () {
+    $(".texto-resumo-livro").hide();
+    $(".btn-close").hide();
+}) */
