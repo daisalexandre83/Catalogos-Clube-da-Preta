e37470684736ccee0c43cf490fa635e741a7b6c3
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-01-29 20:01:36 -0300

    Alterações no HTML, nas classes do CSS e nas funções do Javascript.

--- js/main.js
+++ js/main.js
@@ -8,11 +8,29 @@ function showProducts() {
     document.querySelector('.books').style.display = 'block';
 }
 
-function showResume() {
+function showResume1() {
     console.log('daiane');
-    document.querySelectorAll('.texto-resumo-livro').style.display = 'block';
+    /* document.querySelectorAll('.texto-resumo-livro').style.display = 'block';
     document.querySelector('.btn-escolher').style.display = 'none';
+    document.querySelector('.btn-close').style.display = 'block'; */
+    document.querySelector('.book-informations').style.display = 'block';
+    document.querySelector('.books-2').style.display = 'none'
+    document.querySelector('.books-3').style.display = 'none'
+    document.querySelector('.books-4').style.display = 'none'
+    document.querySelector('.books-5').style.display = 'none'
+    document.querySelector('.books-6').style.display = 'none'
+    document.querySelector('.resumo-livro').style.display = 'none';
     document.querySelector('.btn-close').style.display = 'block';
+}
+
+function close1() {
+    document.querySelector('.book-informations').style.display = 'none';
+    document.querySelector('.books-2').style.display = 'block'
+    document.querySelector('.books-3').style.display = 'block'
+    document.querySelector('.books-4').style.display = 'block'
+    document.querySelector('.books-5').style.display = 'block'
+    document.querySelector('.books-6').style.display = 'block' 
+    document.querySelector('.resumo-livro').style.display = 'block';
 }
 
 /* showProducts();
